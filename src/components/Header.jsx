f34696778754db2fbe1da157aef589d7a25cfae8
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-12-21 20:15:44 +0530

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,7 @@ export default function Header() {
     <header className="fade-up" style={styles.header}>
       <h1>Ankit Mahajan</h1>
       <p>Web Developer</p>
-      <div className="contact">
-        📧 ankitmahajan2257@gmail.com <br />
-        📞 +91 99781 42257 <br />
-        📍 Surat, Gujarat, India <br />
-        🐙 Ankitmahajna022
-      </div>
+    
       <div>
         <a href="mailto:ankitmahajan2257@gmail.com">Email</a>
         <a href="https://github.com/Ankitmahajna022" target="_blank">GitHub</a>
