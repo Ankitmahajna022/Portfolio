2080fdbcfa22e567ea48d694233487dd99be6c26
Ankit Mahajan <ankitmahajan2257@gmail.com>
2025-12-22 12:17:59 +0530

    ./.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,21 @@
-
-
 export default function Projects() {
   const projectList = [
     {
       title: "WhatsApp Chat App",
-      link: "https://github.com/Ankitmahajna022/React-All-Projects/tree/main/whatApp-Chat"
+      link: "https://github.com/Ankitmahajna022/React-All-Projects/tree/main/whatApp-Chat",
     },
     {
       title: "Library Management System",
-      link: "https://github.com/HETVI1405/library-management-system/tree/main/library_management"
+      link: "https://github.com/HETVI1405/library-management-system/tree/main/library_management",
     },
     {
       title: "Tweet Posting App",
-      link: "https://github.com/Ankitmahajna022/Node-js-project/tree/main/Tweet_Posting_App"
+      link: "https://github.com/Ankitmahajna022/Node-js-project/tree/main/Tweet_Posting_App",
     },
     {
       title: "Movie Management System",
-      link: "https://github.com/Ankitmahajna022/Node-js-project/tree/main/Movie_Management_System"
-    }
+      link: "https://github.com/Ankitmahajna022/Node-js-project/tree/main/Movie_Management_System",
+    },
   ];
 
   return (
@@ -25,27 +23,21 @@ export default function Projects() {
       <h2>Projects</h2>
       <div className="projects-container">
         {projectList.map((project, index) => (
-          <Project key={index} title={project.title} link={project.link} />
+          <div className="project-card fade-up" key={index}>
+            <h3>{project.title}</h3>
+            <div className="project-actions">
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-btn"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </section>
-  );
-}
-
-function Project({ title, link }) {
-  return (
-    <div className="project-card fade-up">
-      <h3>{title}</h3>
-      <div className="project-actions">
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="project-btn"
-        >
-          View Code
-        </a>
-      </div>
-    </div>
   );
 }
